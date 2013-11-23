f8e7eec77146cf59fc78b78893f3614b585221b0
Anna <llAnna.Holl@gmail.com>
2013-11-23 16:33:11 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Casino::Application.config.secret_key_base = 'fa7983e1b753a4ab430276660e40b07adb2ab5b6888e163c81a0d649c7c42f9437512d80a06d0247b910e02e114f4d00421ca02361cbbb9d0a395ce80964d620'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Casino::Application.config.session_store :cookie_store, key: '_Casino_session'
